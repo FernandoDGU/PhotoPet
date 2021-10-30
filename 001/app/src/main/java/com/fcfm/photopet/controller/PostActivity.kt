9e9b16b5247d11b45d74a27b889c3b1a15e31c55
FernandoDGU <66134044+FernandoDGU@users.noreply.github.com>
2021-10-29 23:45:29 -0500

    Like Button

--- 001/app/src/main/java/com/fcfm/photopet/controller/PostActivity.kt
+++ 001/app/src/main/java/com/fcfm/photopet/controller/PostActivity.kt
@@ -1,9 +1,12 @@
 package com.fcfm.photopet.controller
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.fcfm.photopet.R
 
 class PostActivity: AppCompatActivity() {
@@ -13,15 +16,34 @@ class PostActivity: AppCompatActivity() {
         setContentView(R.layout.activity_publicacion)
         supportActionBar?.hide()
 
-        val toggle: ToggleButton = findViewById(R.id.toggleButtonLike)
-        toggle.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked){
-                Toast.makeText(applicationContext, "Te gusta", Toast.LENGTH_SHORT).show()
+        val likebtn = findViewById(R.id.BtnLike) as LottieAnimationView
 
-            }else{
-                Toast.makeText(applicationContext, "Ya no te gusta", Toast.LENGTH_SHORT).show()
-            }
+        var like = false
+        likebtn.setOnClickListener{
+            like = likeAnimation(likebtn, R.raw.apple_event, like)
+        }
+    }
+
+
+    private fun likeAnimation(imageView: LottieAnimationView, animation: Int, like:Boolean):Boolean{
+        if(!like){
+            imageView.setAnimation(animation)
+            imageView.playAnimation()
+            Toast.makeText(applicationContext, "Te gusta", Toast.LENGTH_SHORT).show()
+        }else{
+            imageView.animate()
+                .alpha(0f)
+                .setDuration(200)
+                .setListener(object : AnimatorListenerAdapter(){
+                    override fun onAnimationEnd(animation: Animator) {
+                        imageView.setImageResource(R.drawable.twitter_like)
+                        imageView.alpha = 1f
+                    }
+            })
+            Toast.makeText(applicationContext, "Ya no te gusta", Toast.LENGTH_SHORT).show()
 
         }
+
+        return !like
     }
 }
